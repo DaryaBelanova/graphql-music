dcf3acf68be492e79f49dee20d157ae7d4e47c9b
Lyudmila Rezunik <lrezunik@edu.hse.ru>
2024-04-22 02:17:32 +0300

    different ways of showing off the n+1 problem

--- src/main/kotlin/se/hse/musicservicegraphql/musictFeature/song/SongController.kt
+++ src/main/kotlin/se/hse/musicservicegraphql/musictFeature/song/SongController.kt
@@ -1,16 +1,28 @@
 package se.hse.musicservicegraphql.musictFeature.song
 
 import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.graphql.data.method.annotation.SchemaMapping
 import org.springframework.stereotype.Controller
 import se.hse.musicservicegraphql.musictFeature.playlist.Playlist
+import se.hse.musicservicegraphql.musictFeature.playlist.PlaylistRepository
 
 @Controller
-class SongController(private val songRepository: SongRepository) {
+class SongController(private val songRepository: SongRepository, private val playlistRepository: PlaylistRepository) {
 
     @QueryMapping
     fun songs(): List<Song> {
         println("DEBUG: ")
         return songRepository.findAll()
     }
+
+//    @SchemaMapping(typeName = "Song")
+//    fun playlist(song: Song): Playlist? {
+//        println("DEBUG: Retrieve playlist of song ${song.title} by id")
+//        if (song.playlistId != null) {
+//            return playlistRepository.findById(song.playlistId!!).orElse(null)
+//        }
+//
+//        return null
+//    }
 
 }
